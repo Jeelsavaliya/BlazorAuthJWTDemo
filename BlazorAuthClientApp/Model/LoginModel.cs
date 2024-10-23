9fedc81ae64886ee9d3a887b3dd4a99b43064f76
Jeelsavaliya <jeelsavaliya007@gmail.com>
2024-10-23 13:50:30 +0530

    Changes in Login and Register component

--- BlazorAuthClientApp/Model/LoginModel.cs
+++ BlazorAuthClientApp/Model/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazorAuthClientApp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAuthClientApp.Model
 {
     public class LoginModel
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- BlazorAuthClientApp/Pages/Login.razor.cs
+++ BlazorAuthClientApp/Pages/Login.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorAuthClientApp.Model;
 using BlazorAuthClientApp.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 
 namespace BlazorAuthClientApp.Pages
@@ -8,23 +9,46 @@ namespace BlazorAuthClientApp.Pages
     public partial class Login
     {
         [Inject]
+        public AuthenticationStateProvider authenticationStateProvider { get; set; }
+
+        [Inject]
         public AuthService authService { get; set; }
+
         [Inject]
         public NavigationManager navigationManager { get; set; }
 
+       
+
         private LoginModel user = new LoginModel();
 
-        
-        private async Task LoginSuccess()
+        private string errorMessage;
+
+        private bool isAuthenticated;
+
+        protected override async Task OnInitializedAsync()
         {
-            await authService.Login(user);
-            navigationManager.NavigateTo("/home");
+            //If user is authorize and back to login page, login page not to be show
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            isAuthenticated = authState.User.Identity.IsAuthenticated;
+
+            if (isAuthenticated)
+            {
+                navigationManager.NavigateTo("/home");
+            }
         }
 
-        
-        private void GoToRegister()
+
+        private async Task LoginSuccess()
         {
-            navigationManager.NavigateTo("/register");
+            try
+            {
+                await authService.Login(user);
+                navigationManager.NavigateTo("/home");
+            }
+            catch (Exception)
+            {
+                errorMessage = "Login failed";
+            }
         }
     }
 }
